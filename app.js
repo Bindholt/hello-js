d1197b00cda33f8b712e6f9066d01aab2d642977
Bindholt <Cbindholt@gmail.com>
2023-03-09 13:16:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ console.log("app.js is running 🎉");
 
 function sayHello() {
   debugger;
-  let h3 = document.querySelector("h3").textContent;
+  let h3 = document.querySelector("h3");
   let inputName = document.querySelector("#name").value;
   let inputAge = document.querySelector("#age").value;
   let inputEmail = document.querySelector("#email").value;
 
-  h3 = "";
-  inputName ? (h3 += "Hej " + inputName + ".") : "";
+  h3.textContent = "";
+  inputName ? (h3.textContent += "Hej " + inputName + ".") : "";
 
-  inputAge ? (h3 += " Din alder er " + inputAge + ".") : "";
+  inputAge ? (h3.textContent += " Din alder er " + inputAge + ".") : "";
 
-  inputEmail ? (h3 += " Din E-mail er " + inputEmail + ".") : "";
+  inputEmail ? (h3.textContent += " Din E-mail er " + inputEmail + ".") : "";
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
